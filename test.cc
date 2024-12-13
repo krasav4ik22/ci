a297e1b217559c8fa0b9dfb5ddcc3773debd6115
krasav4ik22 <88516426+krasav4ik22@users.noreply.github.com>
2024-12-13 15:38:42 +0300

    Create test.cc

---
+++ test.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    float salary = 56.23;
+    char letter = 'Z';
+    a = 8;
+    b = 34;
+    int c = a+b;
+
+    printf("%d \n", c); // for int
+    printf("%f \n", salary); // for float
+    printf("%c \n", letter); // for char
+
+    return 0;
+}
